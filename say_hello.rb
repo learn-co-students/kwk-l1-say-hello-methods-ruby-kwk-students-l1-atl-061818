7fbcb6f7c2498bfc4a593a41ef7e3266afe5b2e1
yazzyahuja <yahuja21@hb.edu>
2018-06-19 15:24:15 +0000

    Done.

--- say_hello.rb
+++ say_hello.rb
@@ -1,7 +1,7 @@
 # Build your say_hello method here
-def say_hello
-  your_name = gets.chomp
-  puts "Hello #{your_name}!"
+def say_hello(name="Ruby Programmer")
+  puts "Hello #{name}!"
 end
-say_hello
-
+say_hello("Yazzy")
+say_hello("Vidya")
+say_hello("Lexie")
